962a8104d9da7569672c2c9b0f8a83ad800d8cb4
Michael Wohl <mike@gatorriders.com>
2016-03-22 14:21:01 -0400

    fixed a server issue

--- controllers/time.controller.js
+++ controllers/time.controller.js
@@ -1,6 +1,1 @@
-angular.module('timeExample', [])
-  .controller('DateController', ['$scope', function($scope) {
-    $scope.example = {
-      value: new Date(1970, 0, 1, 14, 57, 0)
-    };
-  }]);
+
---
+++ public/js/time.service.js
@@ -0,0 +1,6 @@
+// angular.module('timeExample', [])
+//   .controller('DateController', ['$scope', function($scope) {
+//     $scope.example = {
+//       value: new Date(1970, 0, 1, 14, 57, 0)
+//     };
+//   }]);
